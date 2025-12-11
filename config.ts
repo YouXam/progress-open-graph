fe7a4c908f47726741f7d897c153f47756897606
YouXam <youxam@outlook.com>
2025-12-11 18:28:47 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -51,7 +51,25 @@ export default {
             description: "2025 北京邮电大学暑假",
             start: UTC8("2025-07-05"),
             end: UTC8("2025-08-30"),
-        }
+        },
+        {
+            name: "BUPT 秋季学期",
+            description: "北京邮电大学 2025-2026 第一学期",
+            start: UTC8("2025-09-07"),
+            end: UTC8("2026-01-23"),
+        },
+        {
+            name: "BUPT 寒假",
+            description: "2026 北京邮电大学寒假",
+            start: UTC8("2026-01-24"),
+            end: UTC8("2026-02-28"),
+        },
+        {
+            name: "BUPT 春季学期",
+            description: "北京邮电大学 2025-2026 第二学期",
+            start: UTC8("2026-03-21"),
+            end: UTC8("2026-07-17"),
+        },
     ] as Array<{
         name: string
         description: string
